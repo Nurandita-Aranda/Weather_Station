7726bdcb001bf0be87cb07e225ad092eabf9373f
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2024-05-30 12:44:12 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.set_page_config(
     page_title="Weather Station"
 )
+
+t.markdown("<h1 style='text-align: center; color: raisin black;'>Weather Station</h1>", unsafe_allow_html=True)
