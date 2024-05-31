ce76ab56153eff9f727dfc6801e47b0540505033
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2024-05-31 10:17:37 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import mysql.connector
 
 st.set_page_config(
     page_title="Weather Station"
@@ -6,6 +7,20 @@ st.set_page_config(
 
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Weather Station</h1>", unsafe_allow_html=True)
 
+
+db_config = {
+    'user': 'root',
+    'password': '',
+    'host': '192.168.144.41',
+    'database': 'esp_log'
+}
+
+# Establishing a connection
+try:
+    conn = mysql.connector.connect(**db_config)
+    st.success('Connected to the database successfully!')
+except mysql.connector.Error as err:
+    st.error(f"Error: {err}")
 conn = st.connection('esp_log', type='sql')
 df = conn.query('SELECT Humidity, Temperature, LDR_Value  from log', ttl=600)
 
