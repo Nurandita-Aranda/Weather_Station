363e48bb6047192b73731c65ccb33dc6c9207bea
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2024-05-31 10:13:23 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ st.set_page_config(
 
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Weather Station</h1>", unsafe_allow_html=True)
 
-conn = st.connection('my sql', type='sql')
+conn = st.connection('esp_log', type='sql')
 df = conn.query('SELECT Humidity, Temperature, LDR_Value  from log', ttl=600)
 
 for row in df.itertuples():
