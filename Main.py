a79b14d01806490e35920fbb81df2e1face10f52
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2024-05-31 10:02:08 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,3 +5,9 @@ st.set_page_config(
 )
 
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Weather Station</h1>", unsafe_allow_html=True)
+
+conn = st.connection('my sql', type='sql')
+df = conn.query('SELECT * from mytable', ttl=600)
+
+for row in df.itertuples():
+    st.write(f"
