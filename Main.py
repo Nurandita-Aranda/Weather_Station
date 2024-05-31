dcfebcd62a7ba0bf5c4954fbcfd93e2e9578daea
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2024-05-31 22:44:07 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,15 +8,3 @@ st.set_page_config(
 
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Weather Station</h1>", unsafe_allow_html=True)
 
-connection = mysql.connector.connect(
-    host = "192.168.144.41"
-    port = 3306
-    database = "esp_log"
-    username = "root"
-    password = ""
-)
-
-st.write('connected')
-cursor = connection.cursor()
-cursor.execute('select * from log')
-st.write(cursor.fetchall())
