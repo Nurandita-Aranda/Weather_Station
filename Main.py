0a36f594c4358465a753bd49bb54c2b7c4a64e58
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2024-05-31 10:28:32 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,6 +7,16 @@ st.set_page_config(
 
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Weather Station</h1>", unsafe_allow_html=True)
 
+def connection(name, type):
+    if type == 'sql' and name == 'esp_log':
+        db_config = {
+            'user': 'root',
+            'password': '',
+            'host': 'localhost',
+            'database': 'esp_log'
+        }
+
+st.connection = connection
 conn = st.connection('esp_log', type='sql')
 df = conn.query('SELECT Humidity, Temperature, LDR_Value  from log', ttl=600)
 
