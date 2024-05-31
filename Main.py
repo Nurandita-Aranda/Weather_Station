daaf5798c2e35f2aa66cee14df28909f78ae42b7
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2024-05-31 20:37:25 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import mysql.connector
-import pandas as pd
 
 st.set_page_config(
     page_title="Weather Station"
@@ -8,49 +6,12 @@ st.set_page_config(
 
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Weather Station</h1>", unsafe_allow_html=True)
 
-def connection(name, type):
-    if type == 'sql' and name == 'esp_log':
-        db_config = {
-            'user': 'root',
-            'password': '',
-            'host': '192.168.144.41',
-            'port': '3306',  # Corrected the key from 'ports' to 'port'
-            'database': 'esp_log'
-        }
-        try:
-            conn = mysql.connector.connect(**db_config)
-            return conn
-        except mysql.connector.Error as err:
-            st.error(f"Error: {err}")
-            return None
-    else:
-        st.error("Invalid connection type or name.")
-        return None
-
-st.connection = connection
+# Initialize connection.
 conn = st.connection('esp_log', type='sql')
 
-if conn:
-    try:
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute('SELECT Humidity, Temperature, LDR_Value FROM log')
-        result = cursor.fetchall()
+# Perform query.
+df = conn.query('SELECT Humidity, Temperature, LDR_Value from mytable;', ttl=600)
 
-        # Convert the result to a pandas DataFrame
-        df = pd.DataFrame(result)
-
-        # Display the DataFrame in Streamlit
-        st.dataframe(df)
-
-        # Display individual rows
-        for row in df.itertuples():
-            st.write(f"Humidity: {row.Humidity}, Temperature: {row.Temperature}, LDR Value: {row.LDR_Value}")
-
-    except mysql.connector.Error as err:
-        st.error(f"Error: {err}")
-    finally:
-        cursor.close()
-        conn.close()
-else:
-    st.error("Failed to connect to the database.")
-
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.Humidity}:{row.Temperature}:{row.LDR_Value}:")
