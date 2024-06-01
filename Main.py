b3a4a9149b09be84dfa722bb7f59b1296055ae55
Nurandita-Aranda <129822180+Nurandita-Aranda@users.noreply.github.com>
2024-06-01 16:21:57 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import mysql.connector
 
 st.set_page_config(
     page_title="Weather Station"
@@ -6,12 +8,21 @@ st.set_page_config(
 
 st.markdown("<h1 style='text-align: center; color: raisin black;'>Weather Station</h1>", unsafe_allow_html=True)
 
-# Initialize connection.
-conn = st.connection('esp_log', type='sql')
 
-# Perform query.
-df = conn.query('SELECT Humidity, Temperature, LDR_Value from log;')
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.Humidity}:{row.Temperature}:{row.LDR_Value}:")
+
+connection = mysql.connector.connect(
+    host='http://192.168.144.41/',
+    user='root',
+    password='',
+    database='esp_log'
+)
+
+
+
+cursor=connection.cursor()
+cursor.execute("Select * from log")
+data=cursor.fetchall()
+
+
+st.title('Streamlit MySQL Connection')
